2b02da3a57583d3fefa72d1eb7723c78185b1b40
Vladyslav-Mihulov <vmihulov@prointegra.com.pl>
2025-10-26 19:35:15 +0100

    kalkulator kredytowy + primefaces

---
+++ JSF_01+primefaces/src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -0,0 +1,90 @@
+package com.jsfcourse.calc;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class CalcBB {
+	private String kwota;
+	private String okres;
+        private String op;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getOkres() {
+		return okres;
+	}
+
+	public void setOkres(String okres) {
+		this.okres = okres;
+	}
+        
+        public String getOp() {
+		return okres;
+	}
+
+	public void setOp(String op) {
+		this.op = op;
+	}
+        
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double kwota = Double.parseDouble(this.kwota);
+			double okres = Double.parseDouble(this.okres);
+                        double op = Double.parseDouble(this.op);
+
+			result = ( kwota + (kwota * (op/100) * okres ) ) / ( okres * 12 );
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+	// Put result in messages on AJAX call
+	public String calc_AJAX() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Misisz placic miesiecznie: " + result, null));
+		}
+		return null;
+	}
+
+	public String info() {
+		return "info";
+	}
+}
